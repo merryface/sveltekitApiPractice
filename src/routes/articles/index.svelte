2e0dc0f0d7e662921a4e1a19ac5bbbe2889ecc30
Tarik <tarikmerrylees@gmail.com>
2022-04-16 11:28:10 +0100

    create articles index page

---
+++ src/routes/articles/index.svelte
@@ -0,0 +1,4 @@
+<div class="Articles">
+  <h1 class="Articles__title">Fake Space Articles</h1>
+  <p class="Articles__text">Here are some articles about Space... not really actually. Although maybe I could sort that out one day...</p>
+</div>
