8935cd40addee8e35d8ad4ac2b3a27a32d2ebd5b
Tarik <tarikmerrylees@gmail.com>
2022-04-16 11:22:54 +0100

    create and style home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to This Fake Blog</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="home__title">Welcome to This Fake Blog</h1>
+<script>
+  import '../styles/root/index.scss'
+</script>
+
+<p class="home__text">This is a fake blog. I'm creating this blog to learn a little bit more about Sveltekit and become more comfortable with API... things.</p>
+
+<ul class="home__list">
+  <li class="home__list-item"><a href="https://kit.svelte.dev/">Sveltekit</a></li>
+  <li class="home__list-item"><a href="https://password-demo.jovianmoon.io/dashboard">Simple Login Site by StephenGunn</a></li>
+  <li class="home__list-item"><a href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9hpM9ARM59Ve3jqcb54dqiP">Amazing Sveltekit Crash course by The Net Ninja</a></li>
+</ul>
