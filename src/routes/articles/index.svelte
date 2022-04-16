66fe4a57f0559c70f9773e03e55c1d5237e12867
Tarik <tarikmerrylees@gmail.com>
2022-04-16 11:33:00 +0100

    add styling for index page

--- src/routes/articles/index.svelte
+++ src/routes/articles/index.svelte
@@ -1,4 +1,15 @@
+<script>
+  import '../../styles/articles/index.scss'
+</script>
+
 <div class="Articles">
   <h1 class="Articles__title">Fake Space Articles</h1>
   <p class="Articles__text">Here are some articles about Space... not really actually. Although maybe I could sort that out one day...</p>
+
+  <ul class="Articles__list">
+    <li class="Articles__list-item"><a href="/">Bruno finally goes home to Mars</a></li>
+    <li class="Articles__list-item"><a href="/">Warp speed... or warped vision>?</a></li>
+    <li class="Articles__list-item"><a href="/">SpaceX launches DogeCoin Satellite</a></li>
+  </ul>
 </div>
+
