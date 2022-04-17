669d12843964765a13f44d8a35bbdb553178833f
Tarik <tarikmerrylees@gmail.com>
2022-04-17 01:08:22 +0100

    add article page

--- src/routes/articles/[id].svelte
+++ src/routes/articles/[id].svelte
@@ -1,3 +1,4 @@
 <div class="Article">
   <h1 class="Article__title">This is an Article</h1>
+  <p class="Article__body">This is an article's content</p>
 </div>
--- src/routes/articles/index.svelte
+++ src/routes/articles/index.svelte
@@ -7,9 +7,9 @@
   <p class="Articles__text">Here are some articles about Space... not really actually. Although maybe I could sort that out one day...</p>
 
   <ul class="Articles__list">
-    <a href="/"><li class="Articles__list-item">Bruno finally goes home to Mars</li></a>
-    <a href="/"><li class="Articles__list-item">Warp speed... or warped vision?</li></a>
-    <a href="/"><li class="Articles__list-item">SpaceX launches DogeCoin Satellite</li></a>
+    <a href="/articles/1"><li class="Articles__list-item">Bruno finally goes home to Mars</li></a>
+    <a href="/articles/2"><li class="Articles__list-item">Warp speed... or warped vision?</li></a>
+    <a href="/articles/3"><li class="Articles__list-item">SpaceX launches DogeCoin Satellite</li></a>
   </ul>
 </div>
 
