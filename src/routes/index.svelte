a9b3fcc875422fcc88339df8f01d6bef633c5300
Tarik <tarikmerrylees@gmail.com>
2022-04-17 15:49:52 +0100

    add prefetch add articles link to home

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,8 +5,11 @@
 
 <p class="home__text">This is a fake blog. I'm creating this blog to learn a little bit more about Sveltekit and become more comfortable with API... things.</p>
 
+<a href="/articles" class="home__articlesLink">Articles</a>
+
+<h3 class="home__subheading">Related Resources to the Exercise</h3>
 <ul class="home__list">
-  <a href="https://kit.svelte.dev/"><li class="home__list-item">Sveltekit</li></a>
+  <a sveltekit:prefetch href="https://kit.svelte.dev/"><li class="home__list-item">Sveltekit</li></a>
     <a href="https://password-demo.jovianmoon.io/dashboard"><li class="home__list-item">Simple Login Site by StephenGunn</li></a>
   <a href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9hpM9ARM59Ve3jqcb54dqiP"><li class="home__list-item">Amazing Sveltekit Crash course by The Net Ninja</li>
 </ul>
